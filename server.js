bde59121a6dc79bd40214351fff3677dfc7165a2
Lindergithub <113291420+Lindergithub@users.noreply.github.com>
2023-12-25 22:54:48 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
 require('dotenv').config()
 const MONGO_URL = process.env.MONGO_URL
 const PORT = process.env.PORT || 8000
-
 const express = require('express')
+// const router = express.Router()
 const mongoose = require('mongoose')
-const cors = require ('cors')
-const mysql = require ('mysql')
+const cors = require('cors')
+const mysql = require('mysql')
 //const Venue = require('./models/venuemodel')
 const app = express()
 const venueRoute = require('./routes/venueroute')
 app.use(express.json())
 app.use(cors())
 app.use(express.urlencoded({ extended: false }))
-app .use('/api/venues', venueRoute)
+app.use('/api/venues', venueRoute)
 app.get('/', (req, res) => {
     res.send('Hello NODE API')
 })
@@ -111,72 +111,64 @@ mongoose.
     }).catch((error) => {
         console.log(error)
     });
-    const db = mysql.createConnection({
-        user: "root",
-        host: "localhost",
-        password: "ntnu3528",
-        database: "VenueRentSystem",
-      });
-      app.post("/createuser", (req, res) => {
-        const user_name = req.body.sendtobackname;
-        const user_gender = req.body.sendtobackgender;
-        const user_phone = req.body.sendtobackphone;
-        const user_email = req.body.sendtobackemail;
-      
-        db.query(
-          "INSERT INTO user (user_name, user_gender, user_phone, user_email) VALUES (?,?,?,?)",
-          [user_name, user_gender, user_phone, user_email],
-          (err, result) => {
+const db = mysql.createConnection({
+    user: "root",
+    host: "localhost",
+    password: "ntnu3528",
+    database: "VenueRentSystem",
+});
+app.post("/createuser", (req, res) => {
+    const user_name = req.body.sendtobackname;
+    const user_gender = req.body.sendtobackgender;
+    const user_phone = req.body.sendtobackphone;
+    const user_email = req.body.sendtobackemail;
+
+    db.query(
+        "INSERT INTO user (user_name, user_gender, user_phone, user_email) VALUES (?,?,?,?)",
+        [user_name, user_gender, user_phone, user_email],
+        (err, result) => {
             if (err) {
-              console.log(err);
+                console.log(err);
             } else {
-              res.send(result);
+                res.send(result);
             }
-          }
-        );
-      });
-      
-      app.get("/readuser", (req, res) => {
-        db.query("SELECT * FROM user", (err, result) => {
-          if (err) {
+        }
+    );
+});
+
+app.get("/readuser", (req, res) => {
+    db.query("SELECT * FROM user", (err, result) => {
+        if (err) {
             console.log(err);
-          } else {
+        } else {
             res.send(result);
-          }
+        }
+    });
+});
+
+
+
+app.put("/update", (req, res) => {
+    const user_id = req.body.Id;
+    const user_name = req.body.sendtobackname;
+    db.query("UPDATE user SET user_name = ? WHERE user_id = ?",
+        [user_name, user_id],
+        (err, result) => {
+            if (err) {
+                console.log(err)
+            } else {
+                res.send(result)
+            }
         });
-      });
-      
-      app.get("/showuser", (req, res) => {
-        db.query("SELECT * FROM user", (err, result) => {
-          if (err) {
-            console.log(err);
-          } else {
-            res.send(result);
-          }
-        });
-      });
-      
-      app.put("/update", (req,res) => {
-        const user_id =req.body.Id;
-        const user_name = req.body.sendtobackname;
-        db.query("UPDATE user SET user_name = ? WHERE user_id = ?",
-        [user_name, user_id], 
-        (err,result) => {
-          if (err){
+});
+
+app.delete("/delete/:userId", (req, res) => {
+    const Id = req.params.userId;
+    db.query("DELETE FROM user WHERE user_id = ?", Id, (err, result) => {
+        if (err) {
             console.log(err)
-          } else {
-            res.send(result)
-          }
-        });
-      });
-      
-      app.delete("/delete/:userId", (req, res) => {
-        const Id = req.params.userId;
-        db.query("DELETE FROM user WHERE user_id = ?", Id, (err, result) => {
-          if (err) {
-            console.log(err)
-          } else {
+        } else {
             res.send(result);
-          }
-        });
-      });
+        }
+    });
+});
